bcd3d9a5c6dbd613bdaaca1a4e62070fca255fb7
Lei <michael.82.sh@gmail.com>
2020-04-25 22:04:03 +0800

    build should not be in repo

--- SeanParentTalks/coding_samples/fibonacci/build/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/sw-testing/ModernCXX/coding_samples/fibonacci/build/googletest-src
-# Build directory: C:/sw-testing/ModernCXX/coding_samples/fibonacci/build/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- SeanParentTalks/coding_samples/fibonacci/build/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/sw-testing/ModernCXX/coding_samples/fibonacci/build/googletest-src/googlemock
-# Build directory: C:/sw-testing/ModernCXX/coding_samples/fibonacci/build/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
