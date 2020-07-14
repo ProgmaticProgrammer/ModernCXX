e9c7de451f31cee8c8bc3a2bc3bd48cee8553e1f
Lei <michael.82.sh@gmail.com>
2020-07-14 13:19:55 +0800

    more commands

--- calculator/src/backend/Command.hpp
+++ calculator/src/backend/Command.hpp
@@ -13,17 +13,25 @@ class Command{
 public:
     virtual ~Command() {}
 
+    // executes the command
     void execute(){
         checkPreconditionsImpl();
         executeImpl();
     }
 
+    // undoes the command
     void undo(){
         undoImpl();
     }
 
+    // create a polymorphic copy of the command
     Command* clone() const {
         return cloneImpl();
+    }
+
+    // supplies a short help message for the command
+    const char* helpMessage() const {
+        return helpMessageImpl();
     }
 
 protected:
@@ -31,10 +39,15 @@ protected:
     Command(const Command&) = default;
 
 private:
+    // this function is not pure virtual because a command that needs no preconditions
+    // shouldn't be forced to check for any...thus, this defaults to not throwing
     virtual void checkPreconditionsImpl() const {}
+
     virtual void executeImpl() = 0;
     virtual void undoImpl() = 0;
     virtual Command* cloneImpl() const = 0;
+    // all commands should have a short help
+    virtual const char* helpMessageImpl() const noexcept = 0;
 
     Command(Command&&) = delete;
     Command& operator=(const Command&) = delete;
@@ -42,22 +55,57 @@ private:
 };
 
 template<class T>
-class EnterNumber : public Command
+class CalculatorCommand : public Command
+{
+protected:
+    model::Stack<T>& model_;
+    CalculatorCommand(model::Stack<T>& m):model_(m){}
+};
+
+// accepts a number from input and adds it to the stack
+// no preconditions are necessary for this command
+template<class T>
+class EnterNumber : public CalculatorCommand<T>
 {
 public:
-    EnterNumber(T num, model::Stack<T>& m):num_(num),model_(m){}
+    EnterNumber(T num, model::Stack<T>& m):CalculatorCommand<T>(m),num_(num){}
 private:
-    EnterNumber(const EnterNumber& rhs) :
-        Command{rhs},
-        num_(rhs.num_),
-        model_(rhs.model_) {}
-
+    using CalculatorCommand<T>::model_;
     void executeImpl() override {model_.push(num_);}
     void undoImpl() override {model_.pop();}
     Command* cloneImpl() const { return new EnterNumber(*this); }
+    const char* helpMessageImpl() const noexcept override{return "Adds a number to the stack";}
 private:
     T num_;
-    model::Stack<T>& model_;
+};
+
+// swaps two numbers on the stack
+// precondition: must have at least two elements on the stack
+template<class T>
+class SwapTopOfStack : public Command
+{
+public:
+    SwapTopOfStack() { }
+    SwapTopOfStack(const SwapTopOfStack&);
+    ~SwapTopOfStack();
+
+private:
+    SwapTopOfStack(SwapTopOfStack&&) = delete;
+    SwapTopOfStack& operator=(const SwapTopOfStack&) = delete;
+    SwapTopOfStack& operator=(SwapTopOfStack&&) = delete;
+
+    // throws an exception if the stack size is less than two
+    void checkPreconditionsImpl() const override;
+
+    // swaps the top two stack positions
+    void executeImpl() noexcept override;
+
+    // reverses the swap operation
+    void undoImpl() noexcept override;
+
+    SwapTopOfStack* cloneImpl() const override;
+
+    const char* helpMessageImpl() const noexcept override;
 };
 
 }
